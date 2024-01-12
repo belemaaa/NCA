6e732e10d2395f3c97ba426cf9899e43a6198c41
belemaaa <graceitamunoala@gmail.com>
2024-01-12 15:31:27 +0100

    changed chat file position

--- backend/server.js
+++ backend/server.js
@@ -25,14 +25,14 @@ io.on("connection", (socket) => {
         })
 
         socket.on("send_message", (data) => {
-            console.log(data)
             socket.to(data.room).emit("receive_message", data)
         })
 
         socket.on("disconnect", () => {
             console.log("user disconnected", socket.id)
         })
-    } catch(error){
+    }
+    catch (error) {
         console.error("Error during socket connection: ", error)
     }
     
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import io from 'socket.io-client'
 import React, { useState } from 'react'
-import Chat from './components/Chat';
+import Chat from './Chat';
 
 const socket = io.connect("http://localhost:3001")
 
