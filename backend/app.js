15be73b4fe154c6140d322cd47832df911839a31
belemaaa <graceitamunoala@gmail.com>
2024-01-12 09:28:40 +0100

    demodularized app.js

--- backend/app.js
+++ backend/app.js
@@ -8,50 +8,40 @@ const user_routes = require('./api/routes/user')
 const { Server } = require('socket.io')
 const server = require('./server')
 
-const database_connection = () => {
-    try{
-        mongoose.connect('mongodb://localhost:27017/Node-chat-app', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log('Database connected successfully')
-    } catch(error){
-        console.log('Database connection failed')
+// database connection
+try{
+    mongoose.connect('mongodb://localhost:27017/Node-chat-app', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    console.log('Database connected successfully')
+} catch(error){
+    console.log('Database connection failed')
+}
+
+// handle routing
+app.use('/user', user_routes)
+
+// set up all middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(cors)
+
+// establish socket connection
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
     }
-}
-
-const middlewares = () => {
-    app.use(morgan('dev'))
-    app.use(bodyParser.urlencoded({extended: false}))
-    app.use(bodyParser.json())
-    app.use(cors)
-}
-
-const routes = () => {
-    app.use('/user', user_routes)
-}
-
-const socket_connection = () => {
-    const io = new Server(server, {
-        cors: {
-            origin: "http://localhost:3000",
-            methods: ["GET", "POST"],
-        }
+})
+io.on("connection", (socket) => {
+    console.log(socket.id)
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id)
     })
-    io.on("connection", (socket) => {
-        console.log(socket.id)
-        socket.on("disconnect", () => {
-            console.log("user disconnected", socket.id)
-        })
-    })
+})
     
-}
 
 
-
-
-database_connection()
-middlewares()
-routes()
-socket_connection()
 module.exports = app
